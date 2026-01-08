6a7ee91355e65f9f1dc05ec682f94ff19c8b46b5
Olvegb <144316673+Olvegb@users.noreply.github.com>
2026-01-08 14:08:37 +0100

    Lag_disse_filene

--- importingfile.py
+++ importingfile.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import math
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Ridge
--- visualisering_av_data.py
+++ visualisering_av_data.py
@@ -2,3 +2,26 @@ import importingfile as i
 
 df = i.pd.read_csv("data/winequality-red.csv", sep=";")  # or white
 
+features = [c for c in df.columns if c != "quality"]
+n = len(features)
+
+cols = 3
+rows = i.math.ceil(n / cols)
+
+fig, axes = i.plt.subplots(rows, cols, figsize=(5*cols, 4*rows))
+axes = axes.flatten()
+
+y = df["quality"]
+
+for ax, col in zip(axes, features):
+    ax.scatter(df[col], y, alpha=0.4)
+    ax.set_xlabel(col)
+    ax.set_ylabel("quality")
+    ax.set_title(col)
+
+# Skjul tomme plott
+for ax in axes[len(features):]:
+    ax.axis("off")
+
+i.plt.tight_layout()
+i.plt.show()
